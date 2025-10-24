703cc6610ed599781b3620f8ca9c6ab6173f9237
Lucas Freitas <freitas.lucasaf@gmail.com>
2025-10-24 15:31:58 -0300

    Create TesteLucas database and Jogos table

---
+++ teste.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE TesteLucas;
+GO
+USE TesteLucas;
+GO
+CREATE TABLE Jogos (
+    id INT PRIMARY KEY IDENTITY(1,1),
+    nome NVARCHAR(50),
+    categoria NVARCHAR(30),
+    preco DECIMAL(10,2)
+);
+GO
+INSERT INTO Jogos (nome, categoria, preco)
+VALUES
+('FIFA 25', 'Esportes', 249.00),
+('GTA V', 'Ação', 89.99),
+('The Witcher 3', 'RPG', 99.90);
+GO
+SELECT * FROM Jogos;
