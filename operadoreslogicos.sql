f07c5f6a9f4242f5c710f654d8b1a255505b4745
Lucas Freitas <freitas.lucasaf@gmail.com>
2025-11-17 14:48:56 -0300

    Update operadoreslogicos.sql

--- operadoreslogicos.sql
+++ operadoreslogicos.sql
@@ -1,5 +1,5 @@
 /*
-Aula 23 - Tabela Verdade
+Aula 23 e 24 - Tabela Verdade
 Operadores Lógicos AND e OR
 Curso Udemy: O curso completo de Banco de Dados e SQL, sem mistérios!
 Professor: Felipe Mafra
@@ -8,6 +8,13 @@ Professor: Felipe Mafra
 -- Operadores Lógicos
 
 -- OR == OU
+-- OR --> Para que a saída da query seja verdadeira, basta que apenas uma confição seja verdadeira
+
+SHOW DATABASES;
+
+USE PROJETO;
+
+SELECT * FROM CLIENTE;
 
 SELECT NOME, SEXO, ENDERECO FROM CLIENTE
 WHERE
@@ -18,6 +25,7 @@ WHERE
 SEXO = 'F' OR ENDERECO LIKE '%ESTACIO';
 
 -- AND == E
+-- AND --> Para que a saída seja verdadeira todaa as condições precisam ser verdadeiras
 
 SELECT NOME, SEXO ENDERECO FROM CLIENTE
 WHERE
@@ -26,3 +34,57 @@ SEXO = 'M' AND ENDERECO LIKE '%RJ'
 SELECT NOME, SEXO, ENDERECO FROM CLIENTE
 WHERE
 SEXO = 'F' AND ENDERECO LIKE '%ESTACIO'
+
+-- COUNT(*), GROUP BY, performance com operadores Lógicos
+
+-- Contando os registros de uma Tabela
+
+SELECT COUNT(*) AS "Quantidade de Registros da Tabela Cliente" 
+FROM CLIENTE;
+
+-- Operador GROUP BY
+
+SELECT SEXO, COUNT(*)
+FROM CLIENTE;
+
+SELECT * FROM CLIENTE;
+
+SELECT SEXO, COUNT(*)
+FROM CLIENTE
+GROUP BY SEXO;
+
+-- Para contar:
+SELECT COUNT(*) FROM CLIENTE;
+
+SELECT SEXO, COUNT(*) FROM CLIENTE
+GROUP BY SEXO;
+
+SELECT CIDADE, COUNT(*) FROM CLIENTE
+GROUP BY CIDADE;
+
+-- Condição
+
+SEXO = 'F'
+CIDADE = 'RIO DE JANEIRO'
+
+/* 
+Situação - Tratando com OU / OR
+70% Mulheres = SEXO 'F'
+30% Mora no Rio de Janeiro
+*/
+
+SELECT NOME, SEXO, ENDERECO
+FROM CLIENTE
+WHERE SEXO = 'F'
+OR CIDADE = 'RIO DE JANEIRO';
+
+/*
+Situação - Trantando com E / AND
+70% Mulheres = SEXO 'F'
+30% Mora no Rio de Janeiro
+*/
+
+SELECT NOME, SEXO, ENDERECO
+FROM CLIENTE
+WHERE CIDADE = 'RIO DE JANEIRO'
+AND SEXO = 'F';
